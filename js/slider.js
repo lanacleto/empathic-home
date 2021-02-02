81ee43768bff3cf391245d39411614e7cb712f16
Luiz Anacleto <luizfernandoanacleto68@gmail.com>
2021-02-02 08:35:57 -0300

    minimal changes and change the icon main color

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,8 @@
 const fixedDiv = document.querySelector('body > div.div-block-16.closeall')
-const xInFixed = fixedDiv.querySelector('.x-in-fixed')
+const xInFixed = fixedDiv.querySelector('body > div.div-block-16.closeall > img.x-in-fixed')
 const divWithAll = document.querySelector('.div-block-8')
-const leftArrow = document.querySelector('left-arrow')
-const rightArrow = document.querySelector('right-arrow')
+const leftArrow = document.querySelector('.left-arrow')
+const rightArrow = document.querySelector('.right-arrow')
 const img = fixedDiv.querySelector('.img-in-fixed')
 
 divWithAll.addEventListener('click', e => {
@@ -15,17 +15,17 @@ divWithAll.addEventListener('click', e => {
 
 fixedDiv.addEventListener('click', (e) => {
   const el = e.target
-  if(el.getAttribute('src') == 'images/letra-x.svg'){
+  if(el.getAttribute('src') == 'images/600px-Cross_icon_white.svg.png'){
     fixedDiv.style.display = 'none'
   }
-  if(el.getAttribute('src') == 'images/iconmonstr-angel-right-thin.svg'){
+  if(el.getAttribute('src') == 'images/right-white.svg'){
     let currentSrc = img.getAttribute('src')
     const numberImg = currentSrc.slice(7, 8)
     if(numberImg <= 7) {
       img.setAttribute('src', `images/${parseInt(numberImg) + 1}.jpg`)
     }
   }
-  if(el.getAttribute('src') == 'images/iconmonstr-angel-left-thin.svg'){
+  if(el.getAttribute('src') == 'images/left-white.svg'){
     let currentSrc = img.getAttribute('src')
     const numberImg = currentSrc.slice(7, 8)
     
